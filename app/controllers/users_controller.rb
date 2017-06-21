aaf1f23a8bd074496458d389b66ab44e267db853
github username <github email address>
2017-06-21 16:37:32 -0400

    user logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,12 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def destroy
+		session[:user_id] = nil
+		flash[:success] = "You have logged out"
+		redirect_to root_path
+	end
+
 
 	private
 
