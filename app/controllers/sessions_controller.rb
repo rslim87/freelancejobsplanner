49c70007a20c4a5ffdc575706601315b87fbaba6
github username <github email address>
2017-06-21 16:36:51 -0400

    user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,18 @@
 class SessionsController < ApplicationController
-	
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.find_by(email: params[:user][:email])
+		if @user && @user.authenticate(params[:user][:password])
+			session[:user_id] = @user.id
+			flash[:success] = "You have successfully logged in"
+			redirect_to user_path(@user)
+		else
+			flash[:danger] = "There was something wrong with your login information"
+			redirect_to login_path
+		end
+	end
+
 end
