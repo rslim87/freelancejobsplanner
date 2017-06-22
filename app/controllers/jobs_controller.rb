1feeea6922421133efb0994059383014de34b90b
github username <github email address>
2017-06-22 11:26:49 -0400

    job show page, job delete

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,4 +8,14 @@ class JobsController < ApplicationController
 		@job = Job.find_by_id(params[:id])
 	end
 
+	def new
+		@job = Job.new
+	end
+
+	def destroy
+		@job = Job.find_by_id(params[:id])
+		@job.destroy
+		redirect_to jobs_path
+	end
+
 end
