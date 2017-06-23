f327035bd6dd5ea0a33e49daa3290b598bd43d49
github username <github email address>
2017-06-23 15:14:39 -0400

    client create function, client show page and function

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -6,4 +6,28 @@ class ClientsController < ApplicationController
 	def new
 		@client = Client.new
 	end
+
+	def create
+		@client = current_user.clients.build(client_params)
+		if @client.save
+			redirect_to client_path(@client)
+		else
+			
+			redirect_to new_client_path
+		end
+	end
+
+	def show
+		@client = Client.find_by_id(params[:id])
+	end
+
+
+
+	private
+
+	def client_params
+		params.require(:client).permit(:fullname, :street, :apt, :borough, :notes)
+	end
+
 end
+
