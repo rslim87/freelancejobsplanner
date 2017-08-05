33a152bea6a727684ef2985a8fd645b497b9b0f0
github username <github email address>
2017-08-05 14:47:46 -0400

    added UserSerializer

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -1,3 +1,5 @@
 class ClientSerializer < ActiveModel::Serializer
   attributes :id, :fullname, :street, :apt, :borough, :notes, :user_id
+  has_many :jobs
+  belongs_to :user
 end
--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,3 +1,5 @@
 class JobSerializer < ActiveModel::Serializer
   attributes :id, :name, :jobdate, :payrate, :paid, :equipment, :user_id, :client_id
+  belongs_to :client
+  belongs_to :user
 end
