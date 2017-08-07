96632a6a499c53b5eec0a0c18a21abcdbea80002
github username <github email address>
2017-08-07 19:10:56 -0400

    listed all jobs for client on client show page

--- app/assets/javascripts/clients.js
+++ app/assets/javascripts/clients.js
@@ -1,20 +1,11 @@
 $(document).on("click", "a.jobs", function(event){
 	event.preventDefault(); 
-	$("div.allJobs").prepend('List of all Jobs:')
-	$.get("/jobs" + ".json", function(data){
-		var jobs = data
-		jobs.forEach(function(job){
-		var link = "<a href =" + "/jobs/" + job["id"] + ">" + job["name"] + "</a>"	
-			$("div.allJobs ul").append('<li>' + link +'</li>')
-		});
-		$("div.allJobs").css({
-			'border': '2px black solid',
-			'padding': '5px'
-		});	
-		$("div.allJobs ul").css({
-			'list-style': 'none',
-			'padding': '5px',
-			'margin': '5px'
-		});
-	});
+
+	$("div.displayjobs").prepend('List of all Jobs:')
+	var url = $(this).attr('href')
+	 $.get(url + ".json", function(data){
+	 	$.each(data, function(index){
+	 		$("div.displayjobs ul").append('<li>' + data[index].name + '</li>')
+	 	});
+	 });
 });
